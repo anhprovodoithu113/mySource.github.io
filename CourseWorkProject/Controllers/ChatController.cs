4df5d7fd749bef3fa78aabc576d2d20aa7ad760b
{Michael} <{ Michael }>
2020-04-11 07:13:53 -0600

    Add project files.

---
+++ CourseWorkProject/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using CourseWorkProject.DAL;
+using CourseWorkProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseWorkProject.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        CWContext db = new CWContext();
+        public ActionResult Index()
+        {
+           var CurrentName = HttpContext.User.Identity.Name;
+            var CurrentUser = db.Users.FirstOrDefault(h => h.UserName == CurrentName);
+            if (CurrentUser.Role.id == 2)
+            {
+                var GroupList = db.Groups.Where(h => h.id == CurrentUser.Group.id).FirstOrDefault();
+                var myList = db.Users.Where(h => h.UserName != CurrentName && h.GroupMember.Group.id == GroupList.id).ToList();
+                return View(myList);
+            }
+            var GroupMem = db.GroupMembers.Where(h => h.Group.id == CurrentUser.GroupMember.Group.id).FirstOrDefault();
+            var list = db.Users.Where(h => h.UserName != CurrentName && h.GroupMember.Group.id == GroupMem.Group.id || h.UserName != CurrentName && h.Group.id == GroupMem.Group.id).ToList();
+            return View(list);
+        }
+    }
+}
